b4cf3d306b5394aa55b469ccdc291ce18c74032f
Sebastian Dechert <p212121@googlemail.com>
2019-01-09 07:15:32 +0100

    Update orca_ir.m

--- orca-ir/orca_ir.m
+++ orca-ir/orca_ir.m
@@ -1,6 +1,18 @@
 % plots IR spectrum from orca.out
 % stick spectra + Gaussian, Lorentzian & Pseudo-Voigt line broadening
 % several options
+%
+% 1. Check the options.
+% 2. Run the script with F5. 
+% 3. Open an ORCA output file.
+% 4. Spectrum will be saved as 'ir-spectrum.png' in the same folder
+%    - You need write permissions!
+%    - A previous 'ir-spectrum.png' file will be overwritten!
+% 
+%  Replace 'IR SPECTRUM' with 'RAMAN SECTRUM' in the line 
+%  is_ir_spectrum=strfind(line,'IR SPECTRUM');
+%  for the raman spectrum.
+%
 
 clear all;
 
@@ -128,7 +140,7 @@ if gaussian_ls
     % area or line plot
     if gauss_area
         ar=area(gauss_sum);
-        ar.FaceColor= area_color;
+        ar.FaceColor = area_color;
         ar.EdgeColor = [0 0 0];
         ar.FaceAlpha = 0.2;
         ar.EdgeAlpha = 0.6;
@@ -404,3 +416,33 @@ for i=1:length(v)
   end
 end
 end
+
+% BSD 3-Clause License
+% 
+% Copyright (c) 2019, Sebastian Dechert
+% All rights reserved.
+% 
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are met:
+% 
+% * Redistributions of source code must retain the above copyright notice, this
+%   list of conditions and the following disclaimer.
+% 
+% * Redistributions in binary form must reproduce the above copyright notice,
+%   this list of conditions and the following disclaimer in the documentation
+%   and/or other materials provided with the distribution.
+% 
+% * Neither the name of the copyright holder nor the names of its
+%   contributors may be used to endorse or promote products derived from
+%   this software without specific prior written permission.
+% 
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+% DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+% FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+% DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+% SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+% CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+% OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+% OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
